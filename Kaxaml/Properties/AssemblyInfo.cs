ad4b0fac359a5f446e0440da6d4527a364ca90a8
Jan Karger <punkerat76@gmail.com>
2017-11-22 22:55:29 +0100

    Update AssemblyInfo

--- Kaxaml/Properties/AssemblyInfo.cs
+++ Kaxaml/Properties/AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kaxaml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Kaxaml the lightweight XAML editor.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Kaxaml")]
-[assembly: AssemblyCopyright("Copyright @  2007")]
+[assembly: AssemblyCopyright("Copyright @ 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -59,4 +59,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
